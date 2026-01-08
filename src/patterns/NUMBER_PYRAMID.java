406de22287a877ba7218fe21067297982c175b8e
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-08 21:55:55 +0530

    Create NUMBER_PYRAMID.java

---
+++ src/patterns/NUMBER_PYRAMID.java
@@ -0,0 +1,29 @@
+package patterns;
+
+public class NUMBER_PYRAMID {
+
+
+        public static void numberPyramid(int n) {
+
+            for(int i = 1; i <= n; i++) {
+
+                // spaces
+                for(int j = 1; j <= (n - i); j++) {
+                    System.out.print(" ");
+                }
+
+                // numbers
+                for(int j = 1; j <= i; j++) {
+                    System.out.print(i + " ");
+                }
+
+                System.out.println();
+            }
+        }
+
+        public static void main(String[] args) {
+            numberPyramid(5);
+        }
+    }
+
+
