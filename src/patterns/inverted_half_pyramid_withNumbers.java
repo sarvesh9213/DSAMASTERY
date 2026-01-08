bf56cd258736786d125d6425dfca08b0ffafbaf7
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-08 19:53:09 +0530

    Update inverted_half_pyramid_withNumbers.java

--- src/patterns/inverted_half_pyramid_withNumbers.java
+++ src/patterns/inverted_half_pyramid_withNumbers.java
@@ -16,7 +16,31 @@ public class inverted_half_pyramid_withNumbers {
     }
 
     public static void main(String[] args) {
-        inverted_half_pyramid_withNumber(213
+        inverted_half_pyramid_withNumber(21
         );
     }
 }
+//"C:\Program Files\Java\jdk-24\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2025.1.2\lib\idea_rt.jar=54840" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\sarve\Downloads\Telegram Desktop\DSAMASTERY\out\production\DSAMASTERY" patterns.inverted_half_pyramid_withNumbers
+//1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+//        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+//        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+//        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+//        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+//        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+//        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+//        1 2 3 4 5 6 7 8 9 10 11 12 13 14
+//        1 2 3 4 5 6 7 8 9 10 11 12 13
+//        1 2 3 4 5 6 7 8 9 10 11 12
+//        1 2 3 4 5 6 7 8 9 10 11
+//        1 2 3 4 5 6 7 8 9 10
+//        1 2 3 4 5 6 7 8 9
+//        1 2 3 4 5 6 7 8
+//        1 2 3 4 5 6 7
+//        1 2 3 4 5 6
+//        1 2 3 4 5
+//        1 2 3 4
+//        1 2 3
+//        1 2
+//        1
+//
+//Process finished with exit code 0
