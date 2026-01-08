b4f2be60d8eb6dd8e0d6dbf5836245beac53c8fb
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-08 21:57:34 +0530

    Update PALINDROME.java

--- src/patterns/PALINDROME.java
+++ src/patterns/PALINDROME.java
@@ -1,4 +1,34 @@
 package patterns;
 
 public class PALINDROME {
-}
+
+
+        public static void palindromicNumberPyramid(int n) {
+
+            for(int i = 1; i <= n; i++) {
+
+                // spaces
+                for(int j = 1; j <= (n - i); j++) {
+                    System.out.print(" ");
+                }
+
+                // descending
+                for(int j = i; j >= 1; j--) {
+                    System.out.print(j);
+                }
+
+                // ascending
+                for(int j = 2; j <= i; j++) {
+                    System.out.print(j);
+                }
+
+                System.out.println();
+            }
+        }
+
+        public static void main(String[] args) {
+            palindromicNumberPyramid(5);
+        }
+    }
+
+
