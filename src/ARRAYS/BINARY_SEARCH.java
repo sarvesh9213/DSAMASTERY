94153be9ff6960a03fc4fd3d281aa67d86daf54e
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-14 22:30:51 +0530

    Create BINARY_SEARCH.java

---
+++ src/ARRAYS/BINARY_SEARCH.java
@@ -0,0 +1,42 @@
+package ARRAYS;
+import java.util.*;
+
+public class BINARY_SEARCH {
+   
+
+
+
+        public static int binarySearch(int numbers[], int key) {
+            int start = 0, end = numbers.length - 1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;  // overflow-safe
+
+                if (numbers[mid] == key) {
+                    return mid;
+                }
+
+                if (numbers[mid] < key) {
+                    start = mid + 1;   // move right
+                } else {
+                    end = mid - 1;     // move left
+                }
+            }
+            return -1;   // not found
+        }
+
+        public static void main(String[] args) {
+            int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+            int key = 10;
+
+            int index = binarySearch(numbers, key);
+
+            if (index == -1) {
+                System.out.println("Key not found!");
+            } else {
+                System.out.println("Key found at index: " + index);
+            }
+        }
+    }
+
+
