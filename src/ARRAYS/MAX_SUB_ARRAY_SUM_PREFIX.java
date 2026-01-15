22bd67cc235e4870b93a6d508a9abdbe5cd1ec95
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-15 14:48:27 +0530

    Update MAX_SUB_ARRAY_SUM_PREFIX.java

--- src/ARRAYS/MAX_SUB_ARRAY_SUM_PREFIX.java
+++ src/ARRAYS/MAX_SUB_ARRAY_SUM_PREFIX.java
@@ -18,12 +18,7 @@ public class MAX_SUB_ARRAY_SUM_PREFIX {
             for (int j = i; j < numbers.length; j++) {
                 int end = j;
                 currSum = start==0 ? prefix [end] : prefix[end]-prefix[start-1];
-//                for (int k = start; k <= end; k++) {
-//                    currSum += numbers[k];
-//                }
-
                 System.out.println("Subarray Sum = " + currSum);
-
                 if (maxSum < currSum) {
                     maxSum = currSum;
                 }
