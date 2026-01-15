f7dbe37b2f44a64b297d527619ee8e047c1fafe3
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-15 12:34:03 +0530

    Create SUB_ARRAYS.java

---
+++ src/ARRAYS/SUB_ARRAYS.java
@@ -0,0 +1,41 @@
+package ARRAYS;
+
+public class SUB_ARRAYS {
+
+
+        public static void printSubarrays(int numbers[]) {
+
+            int ts = 0; // total subarrays
+
+            for(int i = 0; i < numbers.length; i++) {
+                int start = i;
+
+                for(int j = i; j < numbers.length; j++) {
+                    int end = j;
+                    int sum = 0;
+                    for(int k = start; k <= end; k++) {
+                        System.out.print(numbers[k] + " ");
+                        sum += numbers[k];
+                       if(k == end){
+                           System.out.println("sum of this subarray: "+ sum);
+                       }
+                    }
+
+                    ts++;
+                    System.out.println(); // new line after each subarray
+                }
+                System.out.println(); // blank line after each starting point
+            }
+
+            System.out.println("Total Subarrays = " + ts);
+        }
+
+        public static void main(String[] args) {
+
+            int numbers[] = {2, 4, 6, 8};
+
+            printSubarrays(numbers);
+        }
+    }
+
+
