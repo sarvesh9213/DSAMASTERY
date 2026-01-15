18cfa6380b6a9645dbdcc06c41636f52088f89da
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-15 13:44:37 +0530

    Update MAX_SUB_ARRAY_SUM_MAM.java

--- src/ARRAYS/MAX_SUB_ARRAY_SUM_MAM.java
+++ src/ARRAYS/MAX_SUB_ARRAY_SUM_MAM.java
@@ -2,62 +2,38 @@ package ARRAYS;
 
 public class MAX_SUB_ARRAY_SUM_MAM {
 
-    public static int findLargest(int arr[]) {
-        int max = arr[0];
-
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    public static int findSmallest(int arr[]) {
-        int min = arr[0];
-
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
-    }
 
 
-    public static void printSubarrays(int numbers[]) {
+        public static void maxSubarraySum(int numbers[]) {
+            int currSum = 0;
+            int maxSum = Integer.MIN_VALUE;
 
-        int ts = 0; // total subarrays
+            for (int i = 0; i < numbers.length; i++) {
+                int start = i;
+                for (int j = i; j < numbers.length; j++) {
+                    int end = j;
+                    currSum = 0;
 
-        for(int i = 0; i < numbers.length; i++) {
-            int start = i;
+                    for (int k = start; k <= end; k++) {
+                        currSum += numbers[k];
+                    }
 
-            for(int j = i; j < numbers.length; j++) {
-                int end = j;
-                int sum = 0;
-                for(int k = start; k <= end; k++) {
-                    System.out.print(numbers[k] + " ");
-                    sum += numbers[k];
-                    if(k == end){
-                        System.out.println("sum of this subarray: "+ sum);
+                    System.out.println("Subarray Sum = " + currSum);
+
+                    if (maxSum < currSum) {
+                        maxSum = currSum;
                     }
                 }
-
-                ts++;
-                System.out.println(); // new line after each subarray
             }
-            System.out.println(); // blank line after each starting point
+
+            System.out.println("Max Subarray Sum = " + maxSum);
         }
 
-        System.out.println("Total Subarrays = " + ts);
+        public static void main(String[] args) {
+            int numbers[] = {2, 4, 6, 8, 10};
+            maxSubarraySum(numbers);
+        }
     }
 
-    public static void main(String[] args) {
-
-        int numbers[] = {2, 4, 6, 8};
-
-        printSubarrays(numbers);
-    }
-}
 
 
