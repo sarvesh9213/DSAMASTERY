78d6e18d2de05c1cfe8b0de2e97e641d896db5c5
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-20 15:04:15 +0530

    Update MAXIMUM_IN_MATRIX.java

--- src/TWO_D_ARRAYS/MAXIMUM_IN_MATRIX.java
+++ src/TWO_D_ARRAYS/MAXIMUM_IN_MATRIX.java
@@ -3,7 +3,7 @@ package TWO_D_ARRAYS;
 import java.util.Scanner;
 
 public class MAXIMUM_IN_MATRIX {
-    public static void input(int n , int[] []matrix) {
+    public static void input(int[] []matrix) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length ; j++) {
@@ -13,7 +13,21 @@ public class MAXIMUM_IN_MATRIX {
 
         }
     }
+
+    public static void MAX_IN_ARRAY(int [][] matrix) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if( matrix [i][j] > max){
+                    max = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("maximun in ur given array is :"+ max);
+    }
     public static void main(String[] args) {
      int [][] matrix = new int [3][3];
+     input(matrix);
+     MAX_IN_ARRAY(matrix);
     }
 }
