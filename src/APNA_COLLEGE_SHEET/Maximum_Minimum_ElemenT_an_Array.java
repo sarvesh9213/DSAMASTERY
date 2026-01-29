89727137f35eb6e9e26ff68f91e17e6f640415b8
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-29 15:39:37 +0530

    Create Maximum_Minimum_ElemenT_an_Array.java

---
+++ src/APNA_COLLEGE_SHEET/Maximum_Minimum_ElemenT_an_Array.java
@@ -0,0 +1,21 @@
+//class Solution {
+//    public ArrayList<Integer> getMinMax(int[] arr) {
+//        // code Here
+//        ArrayList<Integer> result = new ArrayList<> () ;
+//        int min = Integer.MAX_VALUE ;
+//        for (int i = 0 ; i<arr.length ; i++){
+//            if ( arr[ i ] < min ){
+//                min = arr[i];
+//            }
+//        }
+//        result.add(min);
+//        int max = Integer.MIN_VALUE;
+//        for (int i = 0 ; i < arr.length ; i++){
+//            if ( arr[i]>max){
+//                max = arr[i];
+//            }
+//        }
+//        result.add(max);
+//        return result ;
+//    }
+//}
