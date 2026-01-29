c1be89375ef40345ff332e0a833efc0ca0a9834a
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-01-29 15:40:22 +0530

    Create REVERSE_THE_ARRAY.java

---
+++ src/APNA_COLLEGE_SHEET/REVERSE_THE_ARRAY.java
@@ -0,0 +1,15 @@
+//class Solution {
+//    public void reverseArray(int arr[]) {
+//        // code here
+//        int i = 0 ; int n = arr.length - 1 ;
+//        while ( i < n ){
+//            int temp = arr [ n ];
+//            arr [ n ] = arr [ i ];
+//            arr[i]= temp ;
+//            i++;
+//            n--;
+//        }
+//
+//
+//    }
+//}
