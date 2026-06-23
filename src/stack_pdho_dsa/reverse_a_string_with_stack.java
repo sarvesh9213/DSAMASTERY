81048a7bf56df92cbc000a02eead90672bb24e1e
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-06-23 17:33:07 +0530

    Create reverse_a_string_with_stack.java

---
+++ src/stack_pdho_dsa/reverse_a_string_with_stack.java
@@ -0,0 +1,19 @@
+//package stack_pdho_dsa;
+//
+//public class reverse_a_string_with_stack {
+//    Stack<Character> st = new Stack<>();
+//
+//for(int i = 0; i < str.length(); i++)
+//    {
+//        st.push(str.charAt(i));
+//    }
+//
+//    StringBuilder res = new StringBuilder();
+//
+//while(!st.isEmpty())
+//    {
+//        char c = st.peek();
+//        st.pop();
+//        res.append(c);
+//    }
+//}
