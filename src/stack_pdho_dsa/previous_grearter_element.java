94be52fdfb13ba5087424a61c2c65cbc6f2f34ac
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-06-28 14:59:01 +0530

    Create previous_grearter_element.java

---
+++ src/stack_pdho_dsa/previous_grearter_element.java
@@ -0,0 +1,28 @@
+//package stack_pdho_dsa;
+//
+//public class previous_grearter_element {
+//    class Solution {
+//        public int[] previousGreaterElement(int[] arr) {
+//            int n = arr.length;
+//            int[] res = new int[n];
+//            Stack<Integer> st = new Stack<>();
+//
+//            for (int i = 0; i < n; i++) {
+//
+//                while (!st.isEmpty() && st.peek() <= arr[i]) {
+//                    st.pop();
+//                }
+//
+//                if (st.isEmpty()) {
+//                    res[i] = -1;
+//                } else {
+//                    res[i] = st.peek();
+//                }
+//
+//                st.push(arr[i]);
+//            }
+//
+//            return res;
+//        }
+//    }
+//}
