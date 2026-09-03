b5b1b38592fb4642e91ba147dfa64c34a2bca479
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-09-03 23:50:26 +0530

    Create sum_equals_k.java

---
+++ src/PDHO_DSA_PREFIX_SUN/sum_equals_k.java
@@ -0,0 +1,28 @@
+//import java.util.HashMap;
+//
+//class Solution {
+//    public int subarraySum(int[] nums, int k) {
+//
+//        int sum = 0;
+//        int res = 0;
+//
+//        HashMap<Integer, Integer> f = new HashMap<>();
+//
+//        f.put(0, 1);
+//
+//        for (int i = 0; i < nums.length; i++) {
+//
+//            sum += nums[i];
+//
+//            int ques = sum - k;
+//
+//            int freq = f.getOrDefault(ques, 0);
+//
+//            res += freq;
+//
+//            f.put(sum, f.getOrDefault(sum, 0) + 1);
+//        }
+//
+//        return res;
+//    }
+//}
