e108fc7a772e8e5112a17028add13b10c6c00a01
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-09-14 21:13:32 +0530

    Create MAX_in_array.java

---
+++ src/PADHO_RURRSION/MAX_in_array.java
@@ -0,0 +1,12 @@
+//static int fun(int[] a, int n) {
+//
+//    if (n == 1) {
+//        return a[0];
+//    }
+//
+//    int last = a[n - 1];
+//
+//    int answer = fun(a, n - 1);
+//
+//    return Math.max(last, answer);
+//}
